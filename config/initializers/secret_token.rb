220cf8a0db08838a6e5c7ba2c3cf4cf545c99060
Sergey Hanchar <hanchar.sergey@gmail.com>
2013-01-21 17:25:44 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Esiga::Application.config.secret_token = '47d3e9592b8093ca5d6fb9ac2410ebf0ddee1894d48f79d8334df2fb422c615cc95139efa53892a2b444cd083cef59f702c1be7823aba1a4f683fbed60d4972e'
