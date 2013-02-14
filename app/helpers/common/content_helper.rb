86ccf62ccbb6f3fb89b5bea8dcf978b1dcaeb066
Sergey Hanchar <hanchar.sergey@gmail.com>
2013-02-14 20:15:35 +0300

    Correct price format

--- app/helpers/common/content_helper.rb
+++ app/helpers/common/content_helper.rb
@@ -4,4 +4,9 @@ module Common::ContentHelper
     content_tag :div, t('.not_found'), class: 'alert'
   end
 
+  def currency_price(price)
+    number_to_currency price,
+      unit: 'BYR', format: '%n %u', precision: 0
+  end
+
 end
